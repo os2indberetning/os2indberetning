b291bf358c46366ad93c8755a772add4b624f772
Rasmus Lagoni <Rhl@miracle.dk>
2016-08-23 11:23:57 +0200

    login modified

--- Presentation.Web/Controllers/BaseController.cs
+++ Presentation.Web/Controllers/BaseController.cs
@@ -39,9 +39,9 @@ namespace OS2Indberetning.Controllers
             base.Initialize(requestContext);
 
 
-            //string[] httpUser = User.Identity.Name.Split('\\');                
+            string[] httpUser = User.Identity.Name.Split('\\');                
             //TESTETTSTESTESTESTEST
-            string[] httpUser = "MIRACLE\\KBJU".Split('\\');
+            //string[] httpUser = "MIRACLE\\KBJU".Split('\\');
 
             if (httpUser.Length == 2 && String.Equals(httpUser[0], ConfigurationManager.AppSettings["PROTECTED_AD_DOMAIN"], StringComparison.CurrentCultureIgnoreCase))
             {

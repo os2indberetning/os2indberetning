f61588449a6dd14089b046185bf6bff437050e44
martin nielsen fig <martin.fig@hotmail.com>
2016-09-08 18:34:25 +0200

    Now it works after merge

---
+++ Infrastructure.DataAccess/Migrations/201609081618215_changes_after_merge.cs
@@ -0,0 +1,18 @@
+namespace Infrastructure.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class changes_after_merge : DbMigration
+    {
+        public override void Up()
+        {
+         
+        }
+        
+        public override void Down()
+        {
+           
+        }
+    }
+}

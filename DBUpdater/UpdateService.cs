8a1ceb4ed173299a246a3743d4322bda337e5f53
Martin Hvidberg <mhn@miracle.dk>
2016-10-25 09:35:35 +0200

    2.1.2: Logging instead of sending mail about irty adresses.

--- DBUpdater/UpdateService.cs
+++ DBUpdater/UpdateService.cs
@@ -260,16 +260,19 @@ namespace DBUpdater
             _personalAddressRepo.Save();
             _emplRepo.Save();
 
-            Console.WriteLine("Done migrating employees");
+            Console.WriteLine("Before Dirty Adresses");
             var dirtyAddressCount = _cachedRepo.AsQueryable().Count(x => x.IsDirty);
             if (dirtyAddressCount > 0)
             {
                 _logger.Log($"{this.GetType().Name}, MigrateEmployees() There are dirty addresses. ", "DBUpdater", 3);
                 foreach (var admin in _personRepo.AsQueryable().Where(x => x.IsAdmin && x.IsActive))
                 {
-                    _mailSender.SendMail(admin.Mail, "Der er adresser der mangler at blive vasket", "Der mangler at blive vasket " + dirtyAddressCount + "adresser");
+                    //MHN: Mail is disabled until a it is found out which things need to be sent. Until then we just log it. 
+                    _logger.Log($"{this.GetType().Name}, MigrateEmployees() Amount of dirty adresses: " + dirtyAddressCount, "DBUpdater", 3);
+                    //_mailSender.SendMail(admin.Mail, "Der er adresser der mangler at blive vasket", "Der mangler at blive vasket " + dirtyAddressCount + "adresser");
                 }
             }
+            Console.WriteLine("Done migrating employees");
         }
 
         /// <summary>

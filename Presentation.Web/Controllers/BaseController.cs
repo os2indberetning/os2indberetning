76c409d69e41de61b681806a2713018a1989ba18
Martin Hvidberg <mhn@miracle.dk>
2016-11-03 11:32:58 +0100

    Removed hack

--- Presentation.Web/Controllers/BaseController.cs
+++ Presentation.Web/Controllers/BaseController.cs
@@ -36,16 +36,13 @@ namespace OS2Indberetning.Controllers
 
         protected override void Initialize(HttpControllerContext requestContext)
         {
-            _logger.Log("Before initialize. User.Identity.Name=" + User.Identity.Name, "web", 3);
+            _logger.Log("Before initialize Basecontroller. User.Identity.Name=" + User.Identity.Name, "web", 3);
             base.Initialize(requestContext);
 
-            //string[] httpUser = User.Identity.Name.Split('\\');
-            string[] httpUser = new string[2];
-            httpUser[0] = ConfigurationManager.AppSettings["PROTECTED_AD_DOMAIN"];
-            httpUser[1] = "mhn";
+            string[] httpUser = User.Identity.Name.Split('\\');
             //TESTETTSTESTESTESTEST
 
-            _logger.Log("httpuser: " + httpUser, "web", 3);
+            _logger.Log("httpuserinitials: " + httpUser[1] + "httOrganisation: " + httpUser[0], "web", 3);
 
             if (httpUser.Length == 2 && String.Equals(httpUser[0], ConfigurationManager.AppSettings["PROTECTED_AD_DOMAIN"], StringComparison.CurrentCultureIgnoreCase))
             {

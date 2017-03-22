18cff4b27ebcbb1f620290acd4a953108e40b3f4
Rasmus-Rosendal <rro@miracle.dk>
2017-03-22 09:02:57 +0100

    MK column will not be ticked if 4 km rule has been used.

--- Presentation.Web/App/Admin/Controllers/Reports/AdminRejectedReportsController.js
+++ Presentation.Web/App/Admin/Controllers/Reports/AdminRejectedReportsController.js
@@ -192,7 +192,10 @@ angular.module("application").controller("AdminRejectedReportsController", [
                    field: "KilometerAllowance",
                    title: "MK",
                    template: function (data) {
-                       return MkColumnFormatter.format(data);
+                       if (!data.FourKmRule) {
+                           return MkColumnFormatter.format(data);
+                       }
+                       return "";
                    }
                }, {
                    field: "FourKmRule",
--- Presentation.Web/App/MyReports/MyAcceptedReportsController.js
+++ Presentation.Web/App/MyReports/MyAcceptedReportsController.js
@@ -144,7 +144,10 @@ angular.module("application").controller("MyAcceptedReportsController", [
                   field: "KilometerAllowance",
                   title: "MK",
                   template: function (data) {
-                    return MkColumnFormatter.format(data);
+                      if (!data.FourKmRule) {
+                          return MkColumnFormatter.format(data);
+                      }
+                      return "";
                   }
               }, {
                    field: "FourKmRule",

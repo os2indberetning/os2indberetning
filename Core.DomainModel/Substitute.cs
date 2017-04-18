a2db86115dd7b706c44e9df046a8b56692126b12
Rasmus-Rosendal <rro@miracle.dk>
2017-04-18 15:58:48 +0200

    Simplified updating the reports when creating a personal approver.

--- Core.DomainModel/Substitute.cs
+++ Core.DomainModel/Substitute.cs
@@ -9,12 +9,22 @@ namespace Core.DomainModel
         public long StartDateTimestamp { get; set; }
         public long EndDateTimestamp { get; set; }
 
+        /// <summary>
+        /// If this substitute object is modelling a substitute scenario, LeaderId will be the same as PersonId.
+        /// If this substitute object is modelling a personal approver scenario, LeaderId will be the Id of the Substitutes Leader.
+        /// </summary>
         public int LeaderId { get; set; }
         public virtual Person Leader { get; set; }
+        /// <summary>
+        /// The person who is substitute for someone else, and whos going to be approving reports he/she would otherwise not approve.
+        /// </summary>
         public int SubId { get; set; }
         public virtual Person Sub { get; set; }
-        public virtual Person Person { get; set; }
+        /// <summary>
+        /// The person that the substitute is replacing (= being a substitute for).
+        /// </summary>
         public int PersonId { get; set; }
+        public virtual Person Person { get; set; }
         public int OrgUnitId { get; set; }
         public virtual OrgUnit OrgUnit { get; set; }
         public int? CreatedById { get; set; }

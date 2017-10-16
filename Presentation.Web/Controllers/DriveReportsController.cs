b717af6465c214fea67c0cef3800971c5be238ad
Mark Holm <markholm@profibermail.dk>
2017-10-16 12:49:14 +0200

    Changed GetCalculationMethod to handle text value instead of boolean

--- Presentation.Web/Controllers/DriveReportsController.cs
+++ Presentation.Web/Controllers/DriveReportsController.cs
@@ -136,17 +136,25 @@ namespace OS2Indberetning.Controllers
         public IHttpActionResult GetCalculationMethod()
         {
             bool isAltCalc;
-            bool parseSucces = bool.TryParse(ConfigurationManager.AppSettings["AlternativeCalculationMethod"], out isAltCalc);
+            //bool parseSucces = bool.TryParse(ConfigurationManager.AppSettings["AlternativeCalculationMethod"], out isAltCalc);
+            string CalcMethod = ConfigurationManager.AppSettings["AlternativeCalculationMethod"];
+            if (CalcMethod == "")
+                isAltCalc = false;
+            else
+                isAltCalc = true;
+
             _logger.Debug($"{GetType().Name}, GetCalculationMethod(), isAltCalc={isAltCalc}");
 
-            if (parseSucces)
-            {
-                return Ok(isAltCalc);
-            }
-            else
-            {
-                return Ok(false);
-            }
+            return Ok(isAltCalc);
+
+            //if (parseSucces)
+            //{
+            //    return Ok(isAltCalc);
+            //}
+            //else
+            //{
+            //    return Ok(false);
+            //}
 
         }
 

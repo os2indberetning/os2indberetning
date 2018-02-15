a3bf0b8a13252af37007f0e0cd35c2951dd6ac2f
Rasmus-Rosendal <rro@miracle.dk>
2018-02-15 14:48:34 +0100

    Refactored unix-datetime convert methods into a utilities class, and fixed timezone bug.

--- Core.DomainServices/Utilities.cs
+++ Core.DomainServices/Utilities.cs
@@ -10,25 +10,25 @@ namespace Core.DomainServices
     {
 
         /// <summary>
-        /// Converts DateTime to unix timestamp
+        /// Converts DateTime (local timezone) to unix timestamp
         /// </summary>
         /// <param name="date">DateTime to convert</param>
         /// <returns>long timestamp</returns>
         public static long ToUnixTime(DateTime date)
         {
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            return Convert.ToInt64((date - epoch).TotalSeconds);
+            return Convert.ToInt64((TimeZoneInfo.ConvertTimeToUtc(date, TimeZoneInfo.Local) - epoch).TotalSeconds);
         }
 
         /// <summary>
-        /// Converts unix timestamp to datetime
+        /// Converts unix timestamp to datetime (local timezone)
         /// </summary>
         /// <param name="unixTime">Timestamp to convert</param>
         /// <returns>DateTime</returns>
         public static DateTime FromUnixTime(long unixTime)
         {
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            return epoch.AddSeconds(unixTime);
+            return TimeZoneInfo.ConvertTimeFromUtc(epoch.AddSeconds(unixTime), TimeZoneInfo.Local);
         }
     }
 }
---
+++ DomainServices.Test/UtilititesTest.cs
@@ -0,0 +1,70 @@
+﻿using Core.DomainServices;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainServices.Test
+{
+    [TestFixture]
+    public class UtilititesTest
+    {
+        [Test]
+        public void TestToUnixTime_NoDaylightSavings()
+        {
+            // Arrange
+            var datetime = new DateTime(2018, 2, 15, 12, 00, 00);
+            var expectedUnixTimestamp = 1518692400;
+
+            // Act
+            var result = Utilities.ToUnixTime(datetime);
+
+            // Assert
+            Assert.AreEqual(expectedUnixTimestamp, result);
+        }
+
+        [Test]
+        public void TestToUnixTime_DaylightSavings()
+        {
+            // Arrange
+            var datetime = new DateTime(2018, 7, 15, 12, 00, 00);
+            var expectedUnixTimestamp = 1531648800;
+
+            // Act
+            var unixtimestamp = Utilities.ToUnixTime(datetime);
+
+            // Assert
+            Assert.AreEqual(expectedUnixTimestamp, unixtimestamp);
+        }
+
+        [Test]
+        public void TestFromUnixTime_NoDaylightSavings()
+        {
+            // Arrange
+            var unixTimestamp = 1518692400;
+            var expectedDatetime = new DateTime(2018, 2, 15, 12, 00, 00);
+
+            // Act
+            var result = Utilities.FromUnixTime(unixTimestamp);
+
+            // Assert
+            Assert.AreEqual(expectedDatetime, result);
+        }
+
+        [Test]
+        public void TestFromUnixTime_DaylightSavings()
+        {
+            // Arrange
+            var unixTimestamp = 1531648800;
+            var expectedDatetime = new DateTime(2018, 7, 15, 12, 00, 00);
+
+            // Act
+            var result = Utilities.FromUnixTime(unixTimestamp);
+
+            // Assert
+            Assert.AreEqual(expectedDatetime, result);
+        }
+    }
+}

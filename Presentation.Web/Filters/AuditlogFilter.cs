e8440ed56b2354db8a1aeea2d4d7c197820b0da9
Georgi Valentinov Vasov <gvv@miracle.dk>
2018-10-18 14:40:33 +0200

    Vulnerable package update; Nullreference check;

--- Presentation.Web/Filters/AuditlogFilter.cs
+++ Presentation.Web/Filters/AuditlogFilter.cs
@@ -35,19 +35,22 @@ namespace OS2Indberetning.Filters
             try
             {
                 var queryOptions = queryOptionsDictionaryEntry.Value as ODataQueryOptions;
-                Dictionary<string, string> rawValues = new Dictionary<string, string>();
-                rawValues.Add("Count", queryOptions.RawValues.Count);
-                rawValues.Add("Expand", queryOptions.RawValues.Expand);
-                rawValues.Add("Filter", queryOptions.RawValues.Filter);
-                rawValues.Add("Format", queryOptions.RawValues.Format);
-                rawValues.Add("OrderBy", queryOptions.RawValues.OrderBy);
-                rawValues.Add("Select", queryOptions.RawValues.Select);
-                rawValues.Add("Skip", queryOptions.RawValues.Skip);
-                rawValues.Add("SkipToken", queryOptions.RawValues.SkipToken);
-                rawValues.Add("Top", queryOptions.RawValues.Top);
+                if (queryOptions != null)
+                {
+                    Dictionary<string, string> rawValues = new Dictionary<string, string>();
+                    rawValues.Add("Count", queryOptions.RawValues.Count);
+                    rawValues.Add("Expand", queryOptions.RawValues.Expand);
+                    rawValues.Add("Filter", queryOptions.RawValues.Filter);
+                    rawValues.Add("Format", queryOptions.RawValues.Format);
+                    rawValues.Add("OrderBy", queryOptions.RawValues.OrderBy);
+                    rawValues.Add("Select", queryOptions.RawValues.Select);
+                    rawValues.Add("Skip", queryOptions.RawValues.Skip);
+                    rawValues.Add("SkipToken", queryOptions.RawValues.SkipToken);
+                    rawValues.Add("Top", queryOptions.RawValues.Top);
 
-                parameters[queryOptionsDictionaryEntry.Key] = rawValues;
-                jsonParameters = JsonConvert.SerializeObject(parameters);
+                    parameters[queryOptionsDictionaryEntry.Key] = rawValues;
+                    jsonParameters = JsonConvert.SerializeObject(parameters);
+                }
             }
             catch (Exception)
             {

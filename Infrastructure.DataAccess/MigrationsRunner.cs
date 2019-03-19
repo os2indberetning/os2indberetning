4e40f585366aff3bdf43fd2d0946da3af8bbd060
Martin Bæhrenz Bjerregaard <mbb@miracle.dk>
2019-03-19 16:20:51 +0100

    Add DB migrator console application for creating/applying DB migrations on macOS/Mono

---
+++ Infrastructure.DataAccess/MigrationsRunner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data.Entity.Migrations;
+using Infrastructure.DataAccess.Migrations;
+using System.Data.Entity.Migrations.Design;
+using System.IO;
+using System.Resources;
+
+namespace Infrastructure.DataAccess
+{
+    public struct MigrationInfo
+    {
+        public readonly string CodeFileName;
+        public readonly string DesignerFileName;
+        public readonly string ResourceFileName;
+
+        public MigrationInfo(string codeFileName, string designerFileName, string resourceFileName)
+        {
+            CodeFileName = codeFileName;
+            DesignerFileName = designerFileName;
+            ResourceFileName = resourceFileName;
+        }
+    }
+
+    public static class MigrationsRunner
+    {
+        public static void ApplyMigrations()
+        {
+            var configuration = new Configuration();
+            var migrator = new DbMigrator(configuration);
+
+            migrator.Update();
+        }
+
+        public static MigrationInfo CreateMigrations(string migrationName, string migrationDirectory, Func<string, IResourceWriter> createResourceWriter)
+        {
+            var configuration = new Configuration();
+            var scaffolder = new MigrationScaffolder(configuration);
+            var migration = scaffolder.Scaffold(migrationName);
+
+            var codeFileName = migration.MigrationId + ".cs";
+            var designerFileName = migration.MigrationId + ".Designer.cs";
+            var resourceFileName = migration.MigrationId + ".resx";
+
+            File.WriteAllText($"{migrationDirectory}/{codeFileName}", migration.UserCode);
+            File.WriteAllText($"{migrationDirectory}/{designerFileName}", migration.DesignerCode);
+
+            using (var resourceWriter = createResourceWriter($"{migrationDirectory}/{resourceFileName}"))
+            {
+                foreach (var resource in migration.Resources)
+                {
+                    resourceWriter.AddResource(resource.Key, resource.Value);
+                }
+            }
+
+            return new MigrationInfo(codeFileName, designerFileName, resourceFileName);
+        }
+    }
+}

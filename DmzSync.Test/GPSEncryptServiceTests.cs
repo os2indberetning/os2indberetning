fa7dd182af8e0630133270ca60896b5aa615c1f3
Jonas Lauritsen <jol@miracle.dk>
2019-05-13 16:16:04 +0200

    Remove GPSEncrypt reference

--- DmzSync.Test/GPSEncryptServiceTests.cs
+++ DmzSync.Test/GPSEncryptServiceTests.cs
@@ -1,60 +1,59 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Core.ApplicationServices.Logger;
-using Core.DmzModel;
-using Core.DomainServices;
-using Core.DomainServices.Encryption;
-using Infrastructure.DMZGPSEncrypt.Services;
-using NSubstitute;
-using NUnit.Framework;
+﻿//using System.Collections.Generic;
+//using System.Linq;
+//using Core.ApplicationServices.Logger;
+//using Core.DmzModel;
+//using Core.DomainServices;
+//using Core.DomainServices.Encryption;
+//using NSubstitute;
+//using NUnit.Framework;
 
-namespace DmzSync.Test
-{
-    [TestFixture]
-    public class GPSEncryptServiceTests
-    {
-        private GPSEncryptService _gpsEncryptService;
-        private List<Core.DmzModel.GPSCoordinate> _dmzGPSList = new List<Core.DmzModel.GPSCoordinate>();
-        private IGenericRepository<Core.DmzModel.GPSCoordinate> _dmzGPSRepoMock;
-        private ILogger _logger;
+//namespace DmzSync.Test
+//{
+    //[TestFixture]
+    //public class GPSEncryptServiceTests
+    //{
+    //    private GPSEncryptService _gpsEncryptService;
+    //    private List<Core.DmzModel.GPSCoordinate> _dmzGPSList = new List<Core.DmzModel.GPSCoordinate>();
+    //    private IGenericRepository<Core.DmzModel.GPSCoordinate> _dmzGPSRepoMock;
+    //    private ILogger _logger;
 
-        [SetUp]
-        public void SetUp()
-        {            
-            _dmzGPSRepoMock = NSubstitute.Substitute.For<IGenericRepository<Core.DmzModel.GPSCoordinate>>();
-            _logger = NSubstitute.Substitute.For<ILogger>();
+    //    [SetUp]
+    //    public void SetUp()
+    //    {            
+    //        _dmzGPSRepoMock = NSubstitute.Substitute.For<IGenericRepository<Core.DmzModel.GPSCoordinate>>();
+    //        _logger = NSubstitute.Substitute.For<ILogger>();
 
-            _dmzGPSRepoMock.WhenForAnyArgs(x => x.Delete(new Core.DmzModel.GPSCoordinate())).Do(p => _dmzGPSList.Remove(p.Arg<Core.DmzModel.GPSCoordinate>()));
-            _dmzGPSRepoMock.WhenForAnyArgs(x => x.Insert(new Core.DmzModel.GPSCoordinate())).Do(t => _dmzGPSList.Add(t.Arg<Core.DmzModel.GPSCoordinate>()));
+    //        _dmzGPSRepoMock.WhenForAnyArgs(x => x.Delete(new Core.DmzModel.GPSCoordinate())).Do(p => _dmzGPSList.Remove(p.Arg<Core.DmzModel.GPSCoordinate>()));
+    //        _dmzGPSRepoMock.WhenForAnyArgs(x => x.Insert(new Core.DmzModel.GPSCoordinate())).Do(t => _dmzGPSList.Add(t.Arg<Core.DmzModel.GPSCoordinate>()));
 
 
-            _dmzGPSList = new List<GPSCoordinate>()
-            {
-                new GPSCoordinate()
-                {
-                    Latitude = StringCipher.Encrypt("1", Encryptor.EncryptKey),
-                    Longitude = StringCipher.Encrypt("1", Encryptor.EncryptKey),
-                },
-                new GPSCoordinate()
-                {
-                    Latitude = StringCipher.Encrypt("2", Encryptor.EncryptKey),
-                    Longitude = StringCipher.Encrypt("2", Encryptor.EncryptKey),
-                },
-                new GPSCoordinate()
-                {
-                    Latitude = "57.0482206",
-                    Longitude = "9.9193939",
-                },
-                new GPSCoordinate()
-                {
-                    Latitude = "A",
-                    Longitude = "9.9193939",
-                }
-            };
+    //        _dmzGPSList = new List<GPSCoordinate>()
+    //        {
+    //            new GPSCoordinate()
+    //            {
+    //                Latitude = StringCipher.Encrypt("1", Encryptor.EncryptKey),
+    //                Longitude = StringCipher.Encrypt("1", Encryptor.EncryptKey),
+    //            },
+    //            new GPSCoordinate()
+    //            {
+    //                Latitude = StringCipher.Encrypt("2", Encryptor.EncryptKey),
+    //                Longitude = StringCipher.Encrypt("2", Encryptor.EncryptKey),
+    //            },
+    //            new GPSCoordinate()
+    //            {
+    //                Latitude = "57.0482206",
+    //                Longitude = "9.9193939",
+    //            },
+    //            new GPSCoordinate()
+    //            {
+    //                Latitude = "A",
+    //                Longitude = "9.9193939",
+    //            }
+    //        };
 
-            _dmzGPSRepoMock.AsQueryable().ReturnsForAnyArgs(_dmzGPSList.AsQueryable());
-            _gpsEncryptService = new GPSEncryptService(_logger);
-        } 
+    //        _dmzGPSRepoMock.AsQueryable().ReturnsForAnyArgs(_dmzGPSList.AsQueryable());
+    //        _gpsEncryptService = new GPSEncryptService(_logger);
+    //    } 
 
         //[Test]
         //public void GPSEncrypt_ShouldEncrypCoordinate()
@@ -112,6 +111,6 @@ namespace DmzSync.Test
         //    Assert.AreEqual(decrypted.Latitude, lat);
         //    Assert.AreEqual(decrypted.Longitude, lng);
         //}
-    }
-}
+    //}
+//}
     
